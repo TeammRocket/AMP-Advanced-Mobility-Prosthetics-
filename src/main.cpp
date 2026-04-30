f9def65072b4bb1d5e4a6c4eec498d080ed4d822
Nazar <nazar.doskochynskyi@gmail.com>
2026-04-30 16:57:53 +0300

    created testing utility from receiving servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,91 @@
-#include <Arduino.h>
 #include <WiFi.h>
-#include <esp_now.h>
+#include <WebServer.h>
 #include <ESP32Servo.h>
 
-const int PIN_SERVO = 2;
+const char* ssid = "ssid";
+const char* password = "pswd";
+const int servoPin = 2;
 
-Servo receiverServo;
+WebServer server(80);
+Servo myServo;
 
-typedef struct struct_message {
-  int angle;
-  bool isActive;
-} struct_message;
+const char htmlPage[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ESP32 Servo utility</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <style>
+        body { font-family: sans-serif; text-align: center; margin-top: 50px; background-color: #f4f4f9; }
+        input[type=range] { width: 80%; max-width: 400px; height: 25px; }
+        h1 { color: #333; }
+        .angle-display { font-size: 24px; font-weight: bold; color: #0066cc; }
+    </style>
+</head>
+<body>
+    <h1>Servo control</h1>
+    <p>Angle: <span class="angle-display" id="angle_val">90</span>&deg;</p>
+    <input type="range" min="0" max="180" value="90" id="servoSlider" oninput="updateServo(this.value)">
+    
+    <script>
+        function updateServo(val) {
+            document.getElementById("angle_val").innerText = val;
+            fetch('/set?angle=' + val); 
+        }
+    </script>
+</body>
+</html>
+)rawliteral";
 
-struct_message myData;
+void handleRoot() {
+  server.send(200, "text/html", htmlPage);
+}
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&myData, incomingData, sizeof(myData));
-  
-  receiverServo.write(myData.angle);
+void handleSet() {
+  if (server.hasArg("angle")) {
+    int angle = server.arg("angle").toInt();
+    
+    if (angle >= 0 && angle <= 180) {
+      myServo.write(angle);
+    }
+    server.send(200, "text/plain", "OK");
+  } else {
+    server.send(400, "text/plain", "Bad Request");
+  }
 }
 
 void setup() {
   Serial.begin(115200);
-
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  receiverServo.setPeriodHertz(50);
-  receiverServo.attach(PIN_SERVO, 500, 2400);
-  receiverServo.write(0);
-
-  WiFi.mode(WIFI_STA);
-
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("ESP-NOW Error");
-    return;
-  }
-
-  esp_now_register_recv_cb(OnDataRecv);
   
+  myServo.setPeriodHertz(50); 
+  myServo.attach(servoPin, 500, 2400); 
+  myServo.write(90);
+
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("Wi-Fi connected");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/set", handleSet);
+
+  server.begin();
+  Serial.println("server started");
 }
 
 void loop() {
-  delay(1000);
+
+  server.handleClient();
 }
