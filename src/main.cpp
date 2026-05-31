9654894604d4a11ecba491824b7c44b4f4748aaa
Nazar <nazar.doskochynskyi@gmail.com>
2026-06-01 01:03:50 +0300

    fixed esp-now

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,92 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <esp_now.h>
+#include <esp_wifi.h>
 #include <ESP32Servo.h>
 
-// --- PIN CONFIGURATION ---
-const int PIN_SERVO = 2; // Servo motor pin on the receiver board
-
-Servo myServo;
+const int SERVO_PIN = 1; 
+Servo legServo;
 
 typedef struct struct_message {
+  uint8_t type; 
+  uint8_t macAddr[6];
   int angle;
-  bool isActive;
 } struct_message;
 
-struct_message receivedData;
+struct_message incomingData;
+struct_message replyData;
+
+uint8_t senderAddress[6];
+bool isConnected = false;
+
+unsigned long lastMoveTime = 0;
+const unsigned long SERVO_TIMEOUT = 5000; 
+bool isServoAttached = false;
 int currentAngle = 90;
 
-// --- POWER SAVING VARIABLES ---
-bool isServoAttached = false;
-unsigned long lastMoveTime = 0;
-const unsigned long SERVO_TIMEOUT = 500; // Auto-detach after 500ms
+unsigned long lastHopTime = 0;
+int currentChannel = 1;
 
-// --- ESP-NOW CALLBACK ---
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&receivedData, incomingData, sizeof(receivedData));
-  
-  if (currentAngle != receivedData.angle) {
-    currentAngle = receivedData.angle;
+void OnDataRecv(const uint8_t * mac, const uint8_t *incoming, int len) {
+  if (len == sizeof(struct_message)) {
+    memcpy(&incomingData, incoming, sizeof(incomingData));
     
-    if (!isServoAttached) {
-      myServo.attach(PIN_SERVO, 500, 2400);
-      isServoAttached = true;
+    if (incomingData.type == 0 && !isConnected) { 
+      memcpy(senderAddress, incomingData.macAddr, 6);
+      isConnected = true;
+      
+      esp_now_peer_info_t peerInfo;
+      memset(&peerInfo, 0, sizeof(peerInfo)); // Fix for reliable connection
+      memcpy(peerInfo.peer_addr, senderAddress, 6);
+      peerInfo.channel = currentChannel;
+      peerInfo.encrypt = false;
+      esp_now_add_peer(&peerInfo);
+
+      replyData.type = 1; 
+      WiFi.macAddress(replyData.macAddr);
+      esp_now_send(senderAddress, (uint8_t *) &replyData, sizeof(replyData));
+    } 
+    else if (incomingData.type == 2 && isConnected) { 
+      currentAngle = incomingData.angle;
+      
+      if (!isServoAttached) {
+        legServo.attach(SERVO_PIN, 500, 2400);
+        isServoAttached = true;
+      }
+      legServo.write(currentAngle);
+      lastMoveTime = millis();
     }
-    
-    myServo.write(currentAngle);
-    lastMoveTime = millis();
-    
-    Serial.printf("Moving to Angle: %d\n", currentAngle);
   }
 }
 
 void setup() {
   Serial.begin(115200);
-
+  
   WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
 
   ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-  myServo.setPeriodHertz(50);
+  legServo.setPeriodHertz(50); 
   
-  // Test attach and immediately put to sleep
-  myServo.attach(PIN_SERVO, 500, 2400);
-  myServo.write(currentAngle);
-  delay(500); 
-  myServo.detach(); 
-
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("ESP-NOW Init Failed");
-    return;
+  if (esp_now_init() == ESP_OK) {
+    esp_now_register_recv_cb(OnDataRecv);
   }
-  Serial.println("ESP-NOW Receiver Ready.");
-
-  esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop() {
-  // Save power by detaching the servo when not actively moving
-  if (isServoAttached && (millis() - lastMoveTime > SERVO_TIMEOUT)) {
-    myServo.detach();
-    isServoAttached = false;
-    Serial.println("Servo Detached (Power Saving Mode)");
+  if (!isConnected) {
+    // Slower hop (600ms) to ensure it catches the SYNC signal
+    if (millis() - lastHopTime > 600) {
+      currentChannel++;
+      if (currentChannel > 13) currentChannel = 1;
+      esp_wifi_set_channel(currentChannel, WIFI_SECOND_CHAN_NONE);
+      lastHopTime = millis();
+    }
+  } else {
+    if (isServoAttached && (millis() - lastMoveTime > SERVO_TIMEOUT)) {
+      legServo.detach();
+      isServoAttached = false;
+    }
   }
-  
-  delay(10); 
+  delay(10);
 }
